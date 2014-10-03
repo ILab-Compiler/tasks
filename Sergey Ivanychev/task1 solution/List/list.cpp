0b2991289cb74413b237e712a4541d6c044a84b1
Sergey <sergeyivanychev@gmail.com>
2014-10-04 02:51:56 +0400

    Starting to work

---
+++ Sergey Ivanychev/task1 solution/List/list.cpp
@@ -0,0 +1,26 @@
+#include <list.h>
+
+
+/*
+template <class T>
+class DList<T>::Unit
+{
+public:
+                Unit        (T& copied);
+
+    Unit*       next        ();
+    Unit*       prev        ();
+    T&          val         ();
+    bool        ok          ();
+
+    void        print(FILE* stream);
+    void        print();
+private:
+                Unit        ();
+    T*          __val;
+    bool        __empty;
+    Unit*       __next;
+    Unit*       __prev;
+};
+*/
+
