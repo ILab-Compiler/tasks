98338d5179f18753fecbce6b6d5465756fcf4d63
A.Shishpanov <shishpanov2010@yandex.ru>
2015-01-28 21:07:10 +0300

    Added new fields

--- task2/sources/task/graph_impl.h
+++ task2/sources/task/graph_impl.h
@@ -4,6 +4,45 @@
  */
 
 namespace Task {
-
+/*	template < class NodeT, class EdgeT>
+		typename Graph<NodeT, EdgeT>::node_iterator
+		Graph<NodeT, EdgeT>::nodes_begin()
+		{
+			return list_of_nodes.begin();
+		}
+	template < class NodeT, class EdgeT>
+		typename Graph<NodeT, EdgeT>::node_iterator
+		Graph<NodeT, EdgeT>::nodes_end()
+		{
+			return list_of_nodes.end();
+		}
+	template < class NodeT, class EdgeT>
+		typename Graph<NodeT, EdgeT>::edge_iterator
+		Graph<NodeT, EdgeT>::edges_end()
+		{
+			return list_of_edges.end();
+		}
+	template < class NodeT, class EdgeT>
+		typename Graph<NodeT, EdgeT>::edge_iterator
+		Graph<NodeT, EdgeT>::edges_begin()
+		{
+				return list_of_edges.begin();
+		}
+	template < class NodeT, class EdgeT>
+		UInt32 Graph<NodeT, EdgeT>::num_nodes() const
+		{
+			return nodes_amount;
+		}
+	template < class NodeT, class EdgeT>
+		UInt32 Graph<NodeT, EdgeT>::num_edges() const
+		{
+			return edges_amount;
+		}
+	template < class NodeT, class EdgeT>
+		NodeT& Graph<NodeT, EdgeT>::create_node()
+		{
+			
+		}
+		*/
 
 }; // namespace Task
